befca5e42ce308ac205c42b8e919bb2d36b568bf
Pankaj Zade <pankaj.zade@prorigosoftware.com>
2020-06-02 17:40:53 +0530

    assignment no 1

---
+++ logParser/ExportData.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace logParser
+{
+    class ExportData
+    {
+
+        public String logFilePath;
+        public String xcvExportPath;
+        public List<String> Levels = new List<String>();
+
+
+        string getFile(string reusltFilePath)
+        {
+            String filePath = reusltFilePath != null ? reusltFilePath : $"{logFilePath}/result.csv";
+            
+            return filePath;
+        }
+        public void getFilesData()
+        {
+            try
+            {
+                string reusltFilePath = getFile(xcvExportPath);
+                string[] dirs = Directory.GetFiles(logFilePath, "*.log");
+                using (var fileWriter = new StreamWriter(reusltFilePath))
+                    try
+                    {
+                        int counter = 1;
+                        fileWriter.WriteLine("Count, Date, Time, Logging Level, Information");
+                        foreach (string dir in dirs)
+                        {
+                            string[] fileText = readFile(dir);
+                            foreach (var line in fileText)
+                            {
+                                exportToCSV(fileWriter, line, ref counter);
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Something went wrong, Please try again.");
+                    }
+                    finally
+                    {
+                        fileWriter.Close();
+                    }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not find the directory " + e.ToString());
+            }
+        }
+
+        public string[] readFile(String dir)
+        {
+            try
+            {
+                string[] readText = File.ReadAllLines(dir);
+                return readText;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Error in reading file {dir}");
+            }
+            return null;
+        }
+
+        public void exportToCSV(StreamWriter fileWriter, String line, ref int counter)
+        {
+            var info = "";
+            var level = "";
+            var time = "";
+            var date = "";
+
+            var match = Regex.Match(line, @"([^\s]*\s\s)",
+                RegexOptions.IgnoreCase);
+            level = match.Groups[1].Value;
+
+            if (Levels.Contains(level.Trim()))
+            {
+                match = Regex.Match(line, @"(\s+[^\s]*)",
+                RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    time = match.Groups[1].Value;
+
+                }
+
+                match = Regex.Match(line, @"([^\s]+)",
+                RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    date = match.Groups[1].Value;
+                }
+
+                match = Regex.Match(line, @"(\s:.+[^\s]*)",
+                RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    info = match.Groups[1].Value;
+                }
+                var csv = new StringBuilder();
+                fileWriter.WriteLine($"{counter}, {date}, {time}, {level}, {info}");
+                counter++;
+            }
+        }
+    }
+}
---
+++ logParser/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace logParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValid = false;
+            ExportData exportData = new ExportData();
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+
+                switch (args[i])
+                {
+                    case "--log-dir":
+                        {
+                            exportData.logFilePath = args[i + 1];
+                            i++;
+                            isValid = true;
+                            continue;
+                        }
+                    case "--log-level":
+                        {
+                            exportData.Levels.Add(args[i + 1].ToUpper()); ;
+                            i++;
+                            isValid = true;
+                            continue;
+                        }
+                    case "--csv":
+                        {
+                            exportData.xcvExportPath = args[i + 1];
+                            i++;
+                            continue;
+                        }
+                }
+            }
+
+            if (isValid)
+            {
+                exportData.getFilesData();
+            }
+            else if (args[0] == "'--help'")
+            {
+                Console.WriteLine("Usage: logParser --log-dir <dir> --log-level <level> --csv <out>\n --log-dir   Directory to parse recursively for .log files\n    --csv       Out file-path (absolute/relative)");
+            }
+            else { 
+                Console.WriteLine("Please enter valid input.");
+            }
+        }
+    }
+}
